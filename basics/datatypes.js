bbde62b78466c50e9f55b3fe7f66a4f4d8c085b1
DESAIJAY1234 <161826398+DESAIJAY1234@users.noreply.github.com>
2025-05-21 05:08:35 +0000

    data types

---
+++ basics/datatypes.js
@@ -0,0 +1,7 @@
+let name="jay" //string
+let age=20     //INTEGER
+let loggedin=false  //BOOLEAN TRUEOR FALSE
+let state;          //undifine
+let city=null      //null = it an object 
+
+console.log(typeof null)  // to know type of variable
