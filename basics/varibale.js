59029c42d858551cd7d29e7082899e24be39b00b
DESAIJAY1234 <161826398+DESAIJAY1234@users.noreply.github.com>
2025-05-21 04:34:28 +0000

    variable

---
+++ basics/varibale.js
@@ -0,0 +1,9 @@
+const accountId=12344    // difine constent variable  value can't be change after set variable 
+let accountEmail="jay@gmail.com"   //difine variable
+var accountPassword="jaydesai234"  //not use nowadays due to scope problem
+accountCity="ahemadabad" // not prefer in javascript
+let accoutState;
+
+console.log(accountId); // print account id
+console.log([accountId,accountEmail,accountPassword,accountCity,accoutState]) // print all variabel
+console.table([accountId,accountEmail,accountPassword,accountCity,accoutState])//print value intable form
